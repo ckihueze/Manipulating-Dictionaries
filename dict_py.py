4d8a7cfd976c196c373b231d64c1c23bea876589
Chima Ihueze <kihueze@gmail.com>
2021-01-08 18:11:34 +0100

    dict_py

---
+++ dict_py.py
@@ -0,0 +1,25 @@
+"""
+Write an expression that initializes the dictionary my_dict to be the empty dictionary. 
+"""
+
+my_dict = {}
+
+# Tests
+print(type(my_dict))
+print(my_dict)
+
+"""Write an expression that initializes the dictionary my_name to contain two key/value pairs: "Chima" : 1 and "Ihueze" : 2 """
+
+"""
+Template - Create a dictionary my_dict that contains 
+two specified value/pairs 
+"""
+
+my_name = {"Chima": 1, "Ihueze": 2}
+
+# Tests
+print(type(my_name))
+print(my_name["Chima"])
+print(my_name["Ihueze"])
+print(my_name)
+
