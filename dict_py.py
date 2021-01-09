3a0b8340d2ce6813f25573fda4f31af570a630df
Chima Ihueze <kihueze@gmail.com>
2021-01-09 11:22:58 +0100

    dict_py

--- dict_py.py
+++ dict_py.py
@@ -59,6 +59,19 @@ print("Ihueze" in my_names)
 print("Mmesoma" in my_names)
 
 
+"""
+Write a function is_empty(my_names) that
+returns True if a dictionary is empty and False otherwise
+"""
+def is_empty(my_names):
+    
+    """Given a dictionary my_dict, return True if the 
+    dictionary is empty and False otherwise
+    """
+    return len(my_names) == 0
 
-
+# Testing code
+print(is_empty({}))
+print(is_empty({0 : 1}))
+print(is_empty({"Chima" : 1, "Ihueze" : 2}))
 
